610c9d90e8e1d3995ee8df307f87e8cff22e7618
Juanes Capurro <juan.capurro@uniminuto.edu.co>
2025-04-21 17:35:13 -0500

    Creacion de Admin y Cliente

---
+++ src/main/java/com/example/Barberia/models/Cliente.java
@@ -0,0 +1,59 @@
+package com.example.Barberia.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id_cliente;
+    private String nombre;
+    private String celular;
+
+    public Cliente() {
+    }
+
+    public Cliente(long id_cliente, String nombre, String celular) {
+        this.id_cliente = id_cliente;
+        this.nombre = nombre;
+        this.celular = celular;
+    }
+
+    public long getId_cliente() {
+        return id_cliente;
+    }
+
+    public void setId_cliente(long id_cliente) {
+        this.id_cliente = id_cliente;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id_cliente=" + id_cliente +
+                ", nombre='" + nombre + '\'' +
+                ", celular='" + celular + '\'' +
+                '}';
+    }
+
+}
---
+++ src/main/java/com/example/Barberia/repositories/AdministradorService.java
@@ -0,0 +1,5 @@
+package com.example.Barberia.repositories;
+
+public interface AdministradorService {
+
+}
