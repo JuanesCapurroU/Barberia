c7ab774d2677c50012c3255e9ad57b09713c8d79
Juanes Capurro <juan.capurro@uniminuto.edu.co>
2025-05-15 14:48:46 -0500

    Bugs Eliminar Barberos

--- src/main/java/com/example/Barberia/repositories/ReservaRepository.java
+++ src/main/java/com/example/Barberia/repositories/ReservaRepository.java
@@ -1,7 +1,11 @@
 package com.example.Barberia.repositories;
 
 import com.example.Barberia.models.Reserva;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +13,14 @@ import java.util.List;
 @Repository
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
     List<Reserva> findByBarbero_IdBarbero(Long idBarbero);
+
+    // Borra todas las reservas asociadas a un barbero
+    @Transactional
+    @Modifying
+    void deleteByBarbero_IdBarbero(Long idBarbero);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Reserva r WHERE r.horarioDisponible.barbero.idBarbero = :idBarbero")
+    void deleteByHorarioBarberoIdBarbero(@Param("idBarbero") Long idBarbero);
 }
