83073897be62a0e5a0880e9e6df896f313042fc4
Jbustos17 <juandavidbustos04@gmail.com>
2025-05-15 12:23:19 -0500

    solucion error horas

--- src/main/java/com/example/Barberia/controllers/BarberoController.java
+++ src/main/java/com/example/Barberia/controllers/BarberoController.java
@@ -2,9 +2,11 @@ package com.example.Barberia.controllers;
 
 import com.example.Barberia.models.Barbero;
 import com.example.Barberia.services.BarberoServiceImpl;
+import com.example.Barberia.services.HorarioDisponibleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -14,10 +16,21 @@ public class BarberoController {
     @Autowired
     private BarberoServiceImpl barberoServiceImpl;
 
+    @Autowired
+    private HorarioDisponibleService horarioDisponibleService;
+
     @PostMapping
     public Barbero guardarBarbero(@RequestBody Barbero barbero, @RequestParam Long idAdministrador) {
         System.out.println("Barbero recibido: " + barbero);
-        return barberoServiceImpl.guardarBarbero(barbero);
+        Barbero nuevoBarbero = barberoServiceImpl.guardarBarbero(barbero);
+
+        // Crea horarios para los próximos 7 días (ajusta según lo que quieras)
+        for (int i = 0; i < 30; i++) {
+            LocalDate fecha = LocalDate.now().plusDays(i);
+            horarioDisponibleService.crearHorariosParaDiaYBarbero(nuevoBarbero.getIdBarbero(), fecha);
+        }
+
+        return nuevoBarbero;
     }
 
 
@@ -28,6 +41,6 @@ public class BarberoController {
 
     @GetMapping
     public List<Barbero> obtenerTodosLosBarberos() {
-        return barberoServiceImpl.listarBarberos();  // Llamamos al método listarBarberos
+        return barberoServiceImpl.listarBarberos();
     }
 }
